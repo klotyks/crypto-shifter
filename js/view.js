707c3bec761094d23a70cbb727ccf5af7497afcc
klotyks <208618052+klotyks@users.noreply.github.com>
2025-10-10 09:19:36 +0200

    Added: renders,api

---
+++ js/view.js
@@ -0,0 +1,22 @@
+function renderInputTotalBalance(sumUsd) {
+  const elInputText = document.querySelector('#total_balance')
+  elInputText.value = sumUsd
+}
+
+function renderBtc() {
+  // body
+}
+
+function renderEtc() {
+  // body
+}
+
+function renderLtc() {
+  // body
+}
+
+function renderDoge() {
+  // body
+}
+
+const elInputTotalBalance = document.querySelector('#total_balance')
